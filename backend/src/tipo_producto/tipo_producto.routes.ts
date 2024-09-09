b5b8927167a010cb53841ea923c74e9a8125af25
Agusteeng <82476740+Agusteeng@users.noreply.github.com>
2024-09-08 21:33:42 -0300

    Update tipo_producto.routes.ts

--- backend/src/tipo_producto/tipo_producto.routes.ts
+++ backend/src/tipo_producto/tipo_producto.routes.ts
@@ -1,1 +1,10 @@
+import { Router } from 'express'
+import { findAll,findOne,add,update,remove } from './producto.controller.js'
 
+export const productoRouter = Router()
+
+productoRouter.get('/', findAll)
+productoRouter.get('/:id', findOne)
+productoRouter.post('/', add)
+productoRouter.put('/:id', update)
+productoRouter.delete('/:id', remove)
