e7c819c1ad8dc16f9f4dbeb4096560fc23df72f4
Agusteeng <82476740+Agusteeng@users.noreply.github.com>
2024-09-08 21:28:32 -0300

    Update tipo_producto.entity.ts

--- backend/src/tipo_producto/tipo_producto.entity.ts
+++ backend/src/tipo_producto/tipo_producto.entity.ts
@@ -1,1 +1,13 @@
+import { Entity,PrimaryKey,Property } from "@mikro-orm/core"
 
+
+@Entity()
+export class Tipo_producto {
+
+    @PrimaryKey()  
+    id?: int
+
+    @Property()
+    descripcion!: string
+    
+}
