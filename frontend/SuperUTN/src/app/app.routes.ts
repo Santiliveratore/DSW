73222d6dc748f4bbc3b1d325aa9948971ce804aa
Santiago Liveratore <santiliveratore1@gmail.com>
2024-10-02 00:33:43 -0300

    agregando guards para admin y cliente

--- frontend/SuperUTN/src/app/app.routes.ts
+++ frontend/SuperUTN/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AddProductoComponent } from './add-producto/add-producto.component';
 import { EditProductoComponent } from './edit-producto/edit-producto.component';
+import { authGuard,authAdminGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path:'',component:CatalogoComponent},
     {path:'logIn',component:LogInComponent},
     {path:'signUp',component:SignUpComponent},
-    {path:'addProducto',component:AddProductoComponent},
+    {path:'addProducto',component:AddProductoComponent,canActivate:[authAdminGuard]},
     {path:'editProducto/:id',component:EditProductoComponent}
 ];
---
+++ frontend/SuperUTN/src/app/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivateFn,Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const usuarioService= inject(UsuarioService);
+  const router= inject(Router);
+  if(usuarioService.isLoggedIn()){
+    return true
+  }else{
+    router.navigateByUrl('/logIn');
+    return false
+  }
+}
+
+export const authAdminGuard: CanActivateFn = (route, state) => {
+  const usuarioService= inject(UsuarioService);
+  const router= inject(Router);
+  if(usuarioService.isAdmin()){
+    return true
+  }else{
+    router.navigateByUrl('');
+    return false
+  }
+}
